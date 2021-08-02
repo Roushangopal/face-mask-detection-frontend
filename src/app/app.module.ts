c421cd58fbef57fccce9db571c3dd0aef6ad0a7b
sudhanva <sujaypangari1998@gmail.com>
2021-08-03 01:31:27 +0530

    added table for face detection component for API mail process

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
      ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/face-recognition/face-recognition.component.ts
+++ src/app/face-recognition/face-recognition.component.ts
@@ -8,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class FaceRecognitionComponent implements OnInit {
 
   constructor() { }
-
+  public usn = ["1vk17cs047", "1vk17cs061", "1vk17cs032"];
+  public name = ['Roushan Raja', 'Sudhanva Pangari', 'Mizan Farooqui'];
+  public email = ['roushanraja0@gmail.com', 'sujaypangari1998@gmail.com', 'mizanfarooqui@gmail.com'];
+  public phone = ['9060289830', '7348975710', '464486866898'];
   ngOnInit(): void {
   }
-
 }
